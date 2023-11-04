51ebe7d515fa53af2d15f960267aadcc7ed3e5d8
bo.yang <bo.yang@telecwin.com>
2023-11-04 14:40:26 +0800

    Change CMake comments to English

--- toolchain-armv7-32b.cmake
+++ toolchain-armv7-32b.cmake
@@ -1,22 +1,22 @@
-set(CMAKE_SYSTEM_NAME Linux)                          # 操作系统类型
-set(CMAKE_SYSTEM_PROCESSOR arm)                       # CPU 架构
+set(CMAKE_SYSTEM_NAME Linux)                          # Operating system type
+set(CMAKE_SYSTEM_PROCESSOR arm)                       # CPU architecture
 
-set(CMAKE_C_COMPILER arm-linux-musleabihf-gcc)        # gcc 编译器程序名
-set(CMAKE_CXX_COMPILER arm-linux-musleabihf-g++)      # g++ 编译器程序名  
+set(CMAKE_C_COMPILER arm-linux-musleabihf-gcc)        # gcc compiler program name
+set(CMAKE_CXX_COMPILER arm-linux-musleabihf-g++)      # g++ compiler program name
 
-set(CMAKE_FIND_ROOT_PATH /opt/cross/)          # musl g++ 所在根目录
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)    # 告诉CMake要从 ROOT_PATH 下查找g++等程序，但也可以从其他地方查找
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)    # 告诉CMake要从 ROOT_PATH 下查找library库文件
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)    # 告诉CMake要从 ROOT_PATH 下查找头文件
+set(CMAKE_FIND_ROOT_PATH /opt/cross/)          # Root directory of g++
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)    # Tell CMake to look for programs like g++ from under ROOT_PATH, but it can also look elsewhere
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)    # Tell CMake to look for library files from under ROOT_PATH
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)    # Tell CMake to look for header files from under ROOT_PATH
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_C_COMPILER_TARGET arm-linux-musleabihf)    # 使用musl libc 库的目标程序，如果是标准gnu库，可以注释掉本行
+set(CMAKE_C_COMPILER_TARGET arm-linux-musleabihf)    # Target programs that use the musl libc library, if they are standard gnu libraries, can comment out this line
 
 # set(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++")
 
-# 设置编译选项
+# Set compilation options
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a -mfpu=vfpv3")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a -mfpu=vfpv3")    # fpu版本
 
-# 设置链接选项
+# Set link options
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-rpath-link,/opt/cross/arm-linux-musleabihf/lib")
